35b1e3f09fe774f8fb83249c31e4a048cba18f83
Jitesh38 <jp2112003@gmial.com>
2025-03-18 19:24:21 +0530

    added rule book link to events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import events from "./event_detail"
+import events from "./event_detail";
 import { motion } from "framer-motion";
 
 function Events() {
@@ -7,41 +7,58 @@ function Events() {
   const nontechnical_games = events[1];
 
   return (
-   <>
-   <div className="bg-black text-white py-12 px-4">
-   <h1 className="text-center text-5xl md:text-7xl text-yellow-500 zain-bold">Events </h1>
-   <div className="max-w-6xl mx-auto text-center zain-regular">
-        <h3 className="text-2xl font-semibold text-yellow-500 mt-12 mb-6 ">Technical Events</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {technical_games.map((game, index) => (
-            <motion.div
-              key={index}
-              whileHover={{ scale: 1.1 }}
-              className="bg-gray-800 p-6 rounded-lg shadow-lg flex flex-col items-center justify-center transition-all duration-300"
-            >
-              <img src={game.image} alt={game.name} className="w-20 h-20 mb-4 rounded-full" />
-              <h4 className="text-lg font-semibold">{game.name}</h4>
-            </motion.div>
-          ))}
-        </div>
+    <>
+      <div className="bg-black text-white py-12 px-4">
+        <h1 className="text-center text-5xl md:text-7xl text-yellow-500 zain-bold">
+          Events{" "}
+        </h1>
+        <div className="max-w-6xl mx-auto text-center zain-regular">
+          <h3 className="text-2xl font-semibold text-yellow-500 mt-12 mb-6 ">
+            Technical Events
+          </h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+            {technical_games.map((game, index) => (
+              <motion.div
+                key={index}
+                whileHover={{ scale: 1.1 }}
+                className="bg-gray-800 p-6 rounded-lg shadow-lg flex flex-col items-center justify-center transition-all duration-300"
+              >
+                <a href={game.link} target="_blank">
+                  <img
+                    src={game.image}
+                    alt={game.name}
+                    className="w-20 h-20 mb-4 rounded-full"
+                  />
+                  <h4 className="text-lg font-semibold">{game.name}</h4>
+                </a>
+              </motion.div>
+            ))}
+          </div>
 
-        <h3 className="text-2xl font-semibold text-yellow-500 mt-12 mb-6">Non Technical Events</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {nontechnical_games.map((game, index) => (
-            <motion.div
-              key={index}
-              whileHover={{ scale: 1.1 }}
-              className="bg-gray-800 p-6 rounded-lg shadow-lg flex flex-col items-center justify-center transition-all duration-300"
-            >
-              <img src={game.image} alt={game.name} className="w-20 h-20 mb-4 rounded-full" />
-              <h4 className="text-lg font-semibold">{game.name}</h4>
-            </motion.div>
-          ))}
+          <h3 className="text-2xl font-semibold text-yellow-500 mt-12 mb-6">
+            Non Technical Events
+          </h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+            {nontechnical_games.map((game, index) => (
+              <motion.div
+                key={index}
+                whileHover={{ scale: 1.1 }}
+                className="bg-gray-800 p-6 rounded-lg shadow-lg flex flex-col items-center justify-center transition-all duration-300"
+              >
+                <a href={game.link} target="_blank">
+                  <img
+                    src={game.image}
+                    alt={game.name}
+                    className="w-20 h-20 mb-4 rounded-full"
+                  />
+                  <h4 className="text-lg font-semibold">{game.name}</h4>
+                </a>
+              </motion.div>
+            ))}
+          </div>
         </div>
-        </div>
-        </div>
-   
-   </>
+      </div>
+    </>
   );
 }
 
--- src/components/event_detail.js
+++ src/components/event_detail.js
@@ -12,9 +12,9 @@ import Shaan from "../assets/Events/Shaan-e-technomantra.png"
 import Tug from "../assets/Events/Tug of war.png"
 import Web from "../assets/Events/Web wonder.png"
 import Beg from "../assets/Events/Beg borrow steal.png"
-// import ITrangoli from "../assets/It rangoli.png"
-// import ITrangoli from "../assets/It rangoli.png"
-// import ITrangoli from "../assets/It rangoli.png"
+import Querry from "../assets/Events/Query killer.png"
+import Mumble from "../assets/Events/Mumble it.png"
+
 
 
 const event_detail = [
@@ -25,101 +25,101 @@ const event_detail = [
             id: 1,
             name: "IT Rangoli",
             image: ITrangoli,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1VzHbGqD_bYO3kU6S-VMz65Z5z9GGmEj5/view?usp=sharing"
         },
         {
             id: 2,
             name: "Bug Hunt",
             image: Bughunt,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1Xp2JhjwIIVK12wMVVwaEP_HXvOiWwRKZ/view?usp=sharing"
         },
         {
             id: 3,
             name: "Quizzy buzzer",
             image: Quizzy,
-            link: "abc"
+            link: "https://drive.google.com/file/d/15he8N5eqBbcF9VbFnYChlp4Z89KU1V6Q/view?usp=sharing"
         },
         {
             id: 4,
             name: "Poster palette",
             image: Poster,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1jIx3qGN_iFVXSmwuqv6rzrwzfW7PDPVE/view?usp=sharing"
         },
         {
             id: 5,
             name: "Bujo to jaano",
-            image:Guess ,
-            link: "abc"
+            image: Guess,
+            link: "https://drive.google.com/file/d/1EW837Oxsy_4hQ4EFXSE-GN44-RijT7xl/view?usp=sharing"
         },
         {
             id: 6,
             name: "Circuit coder",
             image: Circuit,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1T5j7aj-r6TDqUX505LdexaxcgC6nlSx2/view?usp=sharing"
         },
         {
             id: 7,
             name: "IT preneurship",
             image: ITpreneur,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1Tg7jxxlFnzY7xvKy__K-pLHA-SzwH4Hi/view?usp=sharing"
         },
         {
             id: 8,
             name: "Querry Killer",
-            image: Circuit,
-            link: "abc"
+            image: Querry,
+            link: "https://drive.google.com/file/d/1gAY9CoGs58JQUN2KgioBed0y6ezsiXZz/view?usp=sharing"
         },
         {
             id: 9,
             name: "Shaan-e-technomantra",
             image: Shaan,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1dBjR_dY-u1B7Mp5r1eJwKJLXJsO-Xr0r/view?usp=sharing"
         },
         {
             id: 10,
             name: "Web wonder",
             image: Web,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1kS9Ui6kNINzIkhk84bu4aM0O8v413VSE/view?usp=sharing"
         },
         {
             id: 16,
             name: "Mumble it",
-            image: Circuit,
-            link: "abc"
+            image: Mumble,
+            link: "https://drive.google.com/file/d/19FwoZsayxl0b5op8vP_1NLmppUicPyRe/view?usp=sharing"
         },
     ]
     ,
     [
         //Non technical events
         {
-            id:11,
+            id: 11,
             name: "Cook without fire",
             image: Cook,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1PPu3KPKmxHqO0gloGjnktTyH9g0Rknf6/view?usp=sharing"
         },
         {
             id: 12,
             name: "Beg Borrow Steal",
             image: Beg,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1_vZ3vHo9oiqBOfgobn7mGXUTkGzIrmhf/view?usp=sharing"
         },
         {
             id: 13,
             name: "Quick pick",
             image: Quick,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1cRvDKdnlbq8gIYLy9jGhmXDF6Bgy3XHr/view?usp=sharing"
         },
         {
             id: 14,
             name: "Bgmi",
             image: BGMI,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1eFeseyCI9muHS8FlwxkVpwyr5-o-1Hcz/view?usp=sharing"
         },
         {
             id: 15,
             name: "Tug of war",
             image: Tug,
-            link: "abc"
+            link: "https://drive.google.com/file/d/1jIAZAAAyjjEAgfZtdHVZKOkrXleDoxqL/view?usp=sharing"
         },
     ]
 
