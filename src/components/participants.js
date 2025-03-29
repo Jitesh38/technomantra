de6bc1dcd915f9a7a1e0a4d349a627433841f311
Jitesh38 <jp2112003@gmial.com>
2025-03-29 20:25:54 +0530

    participant name added successfully

--- src/components/participants.js
+++ src/components/participants.js
@@ -11,7 +11,7 @@ const data = [
         id: 2,
         game: "Mumble it",
         participant: [
-           "Vishva Parmar", "Paryul Jain", "Het Gohil", "Charmi Dobariya", "Ritu Vanand", "Khushbu Jadav", "Krishna", "Nehanshi", "Anjali Mehta", "Ashvee Jaiswal", "Anushree Badole", "Zeel Modi", "Patel Dhwani", "Dodiya Chirangi", "Makwana Shruti", "Sanghvi Hirvi", "Shah Khushi", "Ambaliya Dharmik", "Riddhi Panchal", "Dhrumi Thakkar", "Vaishnavi Mali", "Devanshi Raval", "Rathod Riddhi", "Baliwala Shifa", "Patel Rahi", "Gohil Binal", "Meshva", "Saloni", "Yatri Ghohel", "Vnashika Mahakale", "Vaghela Dhwanit", "Jignesh Dhobi", "Chauhan Mahi", "Aaseri Nitu", "Shah Mahek", "Bhavik Rathod", "Patel Kashish", "Krupali Barejiya", "Dishant Patel", "Kaushik Babariya", "Dhara Parmar", "Khushi", "Jiya", "Richa", "Aaradhya", "Dhara", "Dratisti", "Shreya", "Vishwa", "Vidhi", "Shah Vishwa", "Thakkar Miloni", "Solanki Kashish", "Prajapti Avani", "Riddhi Rathod", "Solanki Minal", "Marwadi Meshwa", "Patel Krisha", "Thakor Priyanka", "Mansi", "Dhruvisha", "Modi Honey", "Shreya Prajapati", "Patel Trisha", "Parmar Zalak", "Solanki Nidhi", "Ziya Kalikar", "Rajvi Pansuriya", "Riddhi Malaviya", "Imanshi Manguthiya", "Sakariya Shrushti", "Makawana Aarti", "Kavar Akshi", "Anushka Goswami", "Anjalli Singh"
+            "Vishva Parmar", "Paryul Jain", "Het Gohil", "Charmi Dobariya", "Ritu Vanand", "Khushbu Jadav", "Krishna", "Nehanshi", "Anjali Mehta", "Ashvee Jaiswal", "Anushree Badole", "Zeel Modi", "Patel Dhwani", "Dodiya Chirangi", "Makwana Shruti", "Sanghvi Hirvi", "Shah Khushi", "Ambaliya Dharmik", "Riddhi Panchal", "Dhrumi Thakkar", "Vaishnavi Mali", "Devanshi Raval", "Rathod Riddhi", "Baliwala Shifa", "Patel Rahi", "Gohil Binal", "Meshva", "Saloni", "Yatri Ghohel", "Vnashika Mahakale", "Vaghela Dhwanit", "Jignesh Dhobi", "Chauhan Mahi", "Aaseri Nitu", "Shah Mahek", "Bhavik Rathod", "Patel Kashish", "Krupali Barejiya", "Dishant Patel", "Kaushik Babariya", "Dhara Parmar", "Khushi", "Jiya", "Richa", "Aaradhya", "Dhara", "Dratisti", "Shreya", "Vishwa", "Vidhi", "Shah Vishwa", "Thakkar Miloni", "Solanki Kashish", "Prajapti Avani", "Riddhi Rathod", "Solanki Minal", "Marwadi Meshwa", "Patel Krisha", "Thakor Priyanka", "Mansi", "Dhruvisha", "Modi Honey", "Shreya Prajapati", "Patel Trisha", "Parmar Zalak", "Solanki Nidhi", "Ziya Kalikar", "Rajvi Pansuriya", "Riddhi Malaviya", "Imanshi Manguthiya", "Sakariya Shrushti", "Makawana Aarti", "Kavar Akshi", "Anushka Goswami", "Anjalli Singh"
         ]
     },
     {
@@ -44,7 +44,7 @@ const data = [
         ]
     },
     {
-        id:7,
+        id: 7,
         game: "Query Killer",
         participant: [
             "Pawan Kewlani", "Bhavya Mori", "Mahi Naai", "Aaryan Nayak", "Pathan Rifaut", "Prajapati Dixit", "Patel Aasmi", "Pata Shruti", "Patel Beena", "Jay Mehta", "Tanish Jogu", "Shah Priyansh", "Aanand Mariya", "Jayesh Sonagara", "Tejashvi Kanthra", "Ziya Kalikar", "Makwana Hitesh", "Twisha Thummar", "Varshil Parmar", "Ashutosh Dave", "Shaikh Salik", "Mansuri Noman", "Anand Kanazariya", "Sumit Chetava", "Diya Shah", "Hima Khambhayata", "Shah Honey", "Pranjal", "Riddhesh Raval", "Baliwala Shifa", "Krish", "Rudra", "Priyansh", "Yash Thummar", "Vavaliya Janvi", "Kwity Modi", "Samiksha Jaiswal", "Darji Disha", "Gupta Shivani", "Diya Darji", "Hiren Vaghela", "Dhruv Rapriya", "Darshan Dudhar", "Kathiriya Harsh", "Himesh Myngapati", "Kavya Patel", "Om Patel", "Davra Laksh", "Princy Patel", "Harsoda Mansi", "Malek Nigar", "Joshi Aakanksha", "Chaudhary Kashish", "Vidhi Gohel", "Bharwad Jil", "Barbhaya Jahnvi", "Prajapati Aesha", "Prajapati Shrusti", "Vedanshi Dungrani", "Vidhi Parmar", "Pathan Anamnaaz", "Khatri Krishna", "Vaddoriya Krishna", "Harjani Bhavin", "Rohan Doriya", "Khatik Kashish"
@@ -62,56 +62,249 @@ const data = [
         id: 9,
         game: "IT Rangoli",
         participant: [
-            "Jitesh"
+            "Vaghasiya Mitwa", "Bhesaniya Fenal", "Dobariya Chetu", "Sakhreliya Priyanshi", "Golani Krina", "Axali Lakhani", "Harsh Mistry", "Diya Darji", "Thakkar Miloni", "Thakkar Aangi", "Thakkar Anchal", "Patel Hemal", "Shah Khushi", "Shah Vency", "Soni Mitali", "Hasti Pokal", "Paghdal Swati", "Vekariya Purva", "Padhiyar Nirali", "Nandani Marvadi", "Dhruvi Gohel", "Jinal Prajapati", "Nirali Chauhan"
         ]
     },
     {
         id: 10,
         game: "IT Preneurship",
         participant: [
-            "Jitesh"
+            "Raj Chandel", "Manav Chavla", "Prajapati Jitesh", "Parmar Yash", "Khushi Kalathiya", "Vansh Rathod", "Vansh Balyan", "Aayushi Patel"
         ]
     },
     {
         id: 11,
         game: "Cook Without Fire",
         participant: [
-            "Jitesh"
+            "Gurajar Nirali", "Aditi Gajjar", "Mahek Shah", "Riya Patel", "Moksha Shah", "Thakkar Miloni", "Thakkar Aangi", "Patel Priyanshi", "Panchal Dhruvi", "Manan Modi", "Bhojak Jinendra", "Lewava Zarana", "Chunara Dhruti"
         ]
     },
     {
         id: 12,
         game: "Circuit Coder",
         participant: [
-            "Jitesh"
+            "Darji Shivani",
+            "Parmar Krina",
+            "Vaghela Dhwanit",
+            "Jignesh Dhobi",
+            "Dave Naiya",
+            "Bhambhani Tanisha",
+            "Savaliya Krina",
+            "Prajapati Manish",
+            "Manav Chavla",
+            "Yash Gangvani",
+            "Prajapati Prince",
+            "Darji Vishal",
+            "Het Gohel",
+            "Deep Bhimani",
+            "Harshil Chodvadiya",
+            "Priyanshi Patel",
+            "Vikram Prajapati",
+            "Varshil Parmar",
+            "Darshan Shankhla",
+            "Rathod Bhavik",
+            "Patel Jay",
+            "Vadha Dev",
+            "Prajapati Sweta",
+            "Nayi Riya",
+            "Prajapati Pooja",
+            "Parmar Sneha",
+            "Smit Prajapati",
+            "Akshay Prajapati",
+            "Hiren Vaghela",
+            "Dhruv Rapriya",
+            "Vaishnavi Mali",
+            "Devanshi Raval",
+            "Rana Pratipalsinh",
+            "Parekh Khushal",
+            "Ghadiya Priyanshi",
+            "Harsoda Mansi",
+            "Harjani Bhavin",
+            "Helly Shah"
         ]
     },
     {
         id: 13,
         game: "Bujo To Jaano",
         participant: [
-            "Jitesh"
+            "Riya Koshti",
+            "Dabhi Janvi",
+            "Vishva Prajapati",
+            "Gohil Harshil",
+            "Vidhi Ladani",
+            "Ghoghari Heet",
+            "Prajapati Prince",
+            "Darji Vishal",
+            "Dhruv Kayastha",
+            "Patel Jay",
+            "Vadha Dev",
+            "Patel Ved",
+            "Mahek Shah",
+            "Riya Patel",
+            "Moksha Shah",
+            "Lewava Zarana",
+            "Chunara Dhruti",
+            "Makawana Arya",
+            "Anushree",
+            "Dhrumi",
+            "Zeel",
+            "Patel Raj",
+            "Darbar Yug",
+            "Jain Pratham",
+            "Smit Prajapati",
+            "Akshay Prajapati",
+            "Raj Chandel",
+            "Parekh Khushal",
+            "Bholani Uday",
+            "Trivedi Jaimin",
+            "Panchal Riddhi",
+            "Mali Vaishnavi",
+            "Raval Devanshi",
+            "Thakkar Krisha",
+            "Shah Khushi",
+            "Patel Mitvi",
+            "Dhriti Patel",
+            "Bansari Gajjar",
+            "Bhakti Gohel",
+            "Nishta Shah",
+            "Shruti Jain",
+            "Kashish Patel",
+            "Parmar Vishva",
+            "Jain Prayul",
+            "Patel Princy",
+            "Yatri Ghohel",
+            "Aesha Prajapati",
+            "Vanshika Mahakale",
+            "Parmar Krina",
+            "Panchal Krishna",
+            "Savaliya Krina",
+            "Dhrishi Patel",
+            "Bhavik Rathod",
+            "Umang Rathod",
+            "Patel Tanvi",
+            "Padhiyar Zeel",
+            "Mahi Naai",
+            "Hiya Shah",
+            "Bhavi Shah",
+            "Manav Chawla",
+            "Krupa Rathod",
+            "Darshan Shankhla",
+            "Nakul Thakor"
         ]
     },
     {
         id: 14,
         game: "Bug Hunt",
         participant: [
-            "Jitesh"
+            "Faizan Pathan",
+            "Hinal Mali",
+            "Meet Shrimali",
+            "Rathod Kartik",
+            "Harsh Gohil",
+            "Bhambhani Tanisha",
+            "Riya Koshti",
+            "Vishwa Prajapati",
+            "Dabhi Jhanvi",
+            "Ladani Vidhi",
+            "Riddhi Malaviya",
+            "Makwana Hitesh",
+            "Bhavik Rathod",
+            "Het Gohel",
+            "Harshil Chodavadiya",
+            "Deep Bhimani",
+            "Darshan Shankhla",
+            "Varshil Parmar",
+            "Vikram Prajapati",
+            "Parmar Sneha",
+            "Meet Khatri",
+            "Vishakha Balani",
+            "Riya Bavaliya",
+            "Khushi Prajapati",
+            "Smit Desai",
+            "Pratham Jain",
+            "Hiren Vaghela",
+            "Prajapati Pooja",
+            "Prajapati Pintoo",
+            "Smit Prajapati",
+            "Akshay Prajapati",
+            "Pandya Naiya",
+            "Dhruv Rapriya",
+            "Rana Pratipalsinh",
+            "Parekh Khushal",
+            "Aghera Deep",
+            "Jogani Preet",
+            "Darshan Dudhar",
+            "Harish Prajapati",
+            "Soni Kapil",
+            "Saloni",
+            "Meshva",
+            "Doriya Rohan",
+            "Harjani Bhavin",
+            "Rajput Vishwa",
+            "Yash Gangavani"
         ]
     },
     {
         id: 15,
         game: "BGMI",
         participant: [
-            "Jitesh"
+            "Harsh Vaghasiya",
+            "Krish Kukakdiya",
+            "Bhargav Hingu",
+            "Om Jasoliya",
+            "Patel Mahendra",
+            "Prajapati Kripal",
+            "Patel Raj",
+            "Prajapati Amit",
+            "Kavya Patel",
+            "Om Patel",
+            "Laksh Davra",
+            "Pratham",
+            "Nakul Thakor",
+            "Vikram Prajapati",
+            "Dhruvil",
+            "Nikhil",
+            "Parmar Prince",
+            "Vansh Panchal",
+            "Karan Varma",
+            "Om Raval",
+            "Krish Makawana",
+            "Patel Prathmesh",
+            "Kadiya Vedant",
+            "Dev Valand",
+            "Prince Dodiya",
+            "Yug Dodiya",
+            "Krish Mesariya",
+            "Jatin Parmar",
+            "Vivek Hidad",
+            "Manav Yadav",
+            "Samkit Bhanderi",
+            "Krish Bhagat",
+            "Keyur Bhoi",
+            "Jigar Bhardreshwara",
+            "Harsh Suthar",
+            "Meet Patel",
+            "Meetsinh Rathavi",
+            "Jay Parmar",
+            "Viraj Panchal",
+            "Saumil Thakkar",
+            "Divyang",
+            "Pratham",
+            "Krishna"
         ]
     },
     {
         id: 16,
         game: "Beg Borrow Steal",
         participant: [
-            "Jitesh"
+            "Panchal Krishna", "Darji Shivani", "Savaliya Krina", "Gondliya Riya", "Jainam Sasaniya", "Vihaan Vora", "Harshul Yogi", "Siddhrajsinh Chawda", "Faishal", "Harshil", "Prince", "Vishal", "Jay", "Dixit", "Kalp", "Vanshika", "Pokal Hasti", "Paghadal Swati", "Vekariya Purva", "Padhiyar Nirali", "Prajapati Avani", "Solanki Kashish", "Mistry Vishwa", "Shah Vishwa", "Urja Vaghela", "Aayushi Prajapati", "Riya Koshti", "Krupa Rathod", "Khushi Darji", "Keya Shah", "Nency Modi", "Ritu Valand", "Thakor Priyanka", "Patel Krisha", "Limbachiya Pranav", "Prajapati Kripal", "Kanani Dhruvi", "Chauhan Kisha", "Dadhi Palak", "Patel Tulsi", "Tanish Jogu", "Shah Priyansh", "Aanand Mariya", "Jayesh Sonagara", "Priyanshi Oganda", "Ritu Patel", "Parita Rana", "Rikanksha Khatri", "Marvadi Nandani", "Gohel Dhruvi", "Thakkar Pari", "Prajapati Jinal", "Soni Mitali", "Aasen Nitu", "Chauhan Mahi", "Rathod Karina", "Vaghela Janvi", "Patel Priyanshi", "Kachhiya Upasana", "Prajapti Diya", "Chavda KomalBa", "Dave Naiya", "Parmar Twinkle", "Bhambhani Tanisha", "Savaliya Nisha", "Kothiya Ekta", "Devani Dhruvita", "Sakariya Shrushti", "Bhakti Dhevariya", "Bansi Faldu", "Imanshi Manguthiya", "Mansi Gajera", "Riya Koshti", "Dabhi Janvi", "Vishva Prajapati", "Vidhi Ladani", "Shah Mahek", "Rathod Umang", "Raval Harsh", "Galiya Kashyap", "Vidhi Gohil", "Kashuish Chaudhary", "Kwity Modi", "Akanksha Joshi", "Aditi Gajjar", "Namrata Shidhani", "Meet Rathvi", "Dhruv Kayastha", "Thakkar Aangi", "Thakkar Aachal", "Thakkar Miloni", "Gohil Keval", "Barot Aayuni", "Kathiriya Nensi", "Padhiyar Krupa", "Princy", "Mahek Nakrani", "Jahanvi", "Parthvi", "Riddhi", "Hinal Mali", "Yashvi Limbachiya", "Yashvi Chaudhary", "Ishika Patel", "Pranjal", "Sanya", "Twinkle", "Rahi", "Shifa", "Patel Kashish", "Patel Devanshi", "Panchal Shrushti", "Ubhadiya Hasti", "Pratham Jain", "Harsh Gohil", "Kartik Rathod", "Dhruv Yadav", "Pari", "Vidhi", "Divyanshi", "Hetvi", "Pooja", "Jigi", "Hemakshi", "Vidhi", "Chauhan Devanshi", "Patel Grena", "Shah Aryan", "Panchal Mitanshi", "Chauhan Daksh", "Rathod Kevish", "Vora Jahnvi", "Zalawadiya Drashti", "Chodvadiya Rohan", "Panchal Viraj", "Prajapati Krupesh", "Kathiriya Tirth", "Nishtha Shah", "Manali Patel", "Shruti Jain", "Kashish Patel", "Raxit", "Riya Nagda", "Yash Gagvani", "Zalak Thakkar", "Pranjal Parmar", "Parmar Dhara", "Saloni", "Manav Modi"
+        ]
+    },
+    {
+        id: 17,
+        game: "Free Fire",
+        participant: [
+            "Samir", "Dhruv", "Yug", "Priyanshu", "Pranav Prajapati", "Rana Meet", "Panchal Manthan", "Patel Zeel", "Jainish", "Keneth", "Rudra", "Joyal", "Shubham Chaudhry", "Dipal Rathod", "Aakash", "Mayank", "Mahendra Patel", "Dhvanit Vaghela", "Bhavesh Chauhan", "Rudra Patel", "Dhruv Kayastha", "Kalp Shah", "Harshil Gohil", "Ghoghari Heet", "Chauhan Bhavy", "Prajapati Dhyan", "Harikrishn", "Raviraj Zala", "Manan Modi", "Jatin Chauhan", "Mohit Parmar", "Rajan Chudasama", "Parmar Harshh", "Harsh Prajapati", "Parmar Aditya", "Hardik Barot", "Dev Solanki", "Raj Solanki", "Gohel Devang", "Mitesh Chavda", "Parmar Aditya", "Parth Laniya", "Yash Prajapati", "Sachin Talaviya", "Davra Jay", "Rohan Chodvadiya", "Darji Ketan", "Rohan Patel", "Raval Urav", "Darji Vishal", "Pandav Manav", "Ghaskata Keyur", "Harshil", "Patel Vedant", "Sujal", "Hitarth", "Jignesh Chauhan", "Meet Vora", "Dhruv Vanol", "Het Gajera", "Vishvas Malaviya", "Balar Heet", "Pal Ghelani", "Ojha Ankit", "Aryan Makawana", "Nikul Chavda", "Prince Barot", "Rohan Doriya", "Krunal Bhavsar", "Meet Aacharya", "Kishan Prajapati", "Harsh Shrimali", "Yash Chauhan", "Rehan Vora", "Hardik Jadav"
         ]
     },
 
